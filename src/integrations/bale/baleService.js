c7e1bbb7b9259acf6da1bdc7775f404e8acaec76
amir <hasanabadi.am1380@gmail.com>
2026-05-10 16:57:47 +0330

    Feat: send message added to bale

--- src/integrations/bale/baleService.js
+++ src/integrations/bale/baleService.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 
-const TOKEN = "913651672:gduYv3bBK3nHscslebOlpDHKWunLVi8r7rM"
+const TOKEN = "913651672:gduYv3bBK3nHscslebOlpDHKWunLVi8r7rM";
 const BASE_URL = `https://tapi.bale.ai/bot${TOKEN}`;
 
 async function sendMessage(chatId, text) {
   try {
     const res = await axios.post(`${BASE_URL}/sendMessage`, {
       chat_id: chatId,
-      text: text
+      text: text,
     });
 
     console.log(res.data);
@@ -16,7 +16,26 @@ async function sendMessage(chatId, text) {
   }
 }
 
-sendMessage(686222237, "سلام از Node.js 🚀");
+async function sendForm(chatId) {
+  const text = `
+فرم ثبت اطلاعات
+
+لطفا به سوالات زیر پاسخ دهید:
+
+1️⃣ نام و نام خانوادگی
+2️⃣ شماره موبایل
+3️⃣ ایمیل
+`;
+
+  await axios.post(`${BASE_URL}/sendMessage`, {
+    chat_id: chatId,
+    text: text,
+  });
+}
+
+sendForm(686222237);
+
+// sendMessage(686222237, "سلام از Node.js 🚀");
 // ----------------------------------------------
 // async function getUpdates() {
 //   const res = await axios.get(`${BASE_URL}/getUpdates`);
