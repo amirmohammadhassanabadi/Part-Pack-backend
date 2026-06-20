9ce122684247957bac8f0d77d0c48f46a14188a3
amir <hasanabadi.am1380@gmail.com>
2026-06-20 16:51:08 +0330

    Feat: part category model added

---
+++ src/modules/vehicles/model/partCategory.models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const partCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    slug: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "PartCategory",
+      required: true,
+      index: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("PartCategory", carModelSchema);
