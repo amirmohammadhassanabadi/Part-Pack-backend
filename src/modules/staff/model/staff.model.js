39abcbfedd42d71bb1ae3600c64a279ba3d3971e
amir <hasanabadi.am1380@gmail.com>
2026-06-25 16:28:22 +0330

    Feat: staff model added

---
+++ src/modules/staff/model/staff.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const staffSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "operator"],
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Staff", staffSchema);
