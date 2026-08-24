51b0d12910a9dddce3f60d0112f40b55a2260eae
amir <hasanabadi.am1380@gmail.com>
2026-08-25 02:06:30 +0330

    Feat: openTelemetry added  - [dev]

--- instrumentation.js
+++ instrumentation.js
@@ -7,7 +7,9 @@ const {
 } = require("@opentelemetry/exporter-trace-otlp-proto");
 
 const traceExporter = new OTLPTraceExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || "http://localhost:4318/v1/traces",
+  url:
+    process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
+    "http://localhost:4318/v1/traces",
 });
 
 const sdk = new NodeSDK({
