f3e8a50445e775f4c493b162d25b9135c7111c20
amir <hasanabadi.am1380@gmail.com>
2026-08-25 05:50:51 +0330

    Feat: instrumentation.js optimized - [dev]

--- instrumentation.js
+++ instrumentation.js
@@ -19,7 +19,7 @@ const resource = resourceFromAttributes({
 
 const traceExporter = new OTLPTraceExporter({
   url:
-    process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
+    process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT ||
     "http://localhost:4318/v1/traces",
 });
 
